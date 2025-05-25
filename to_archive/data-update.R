f89e0fcab6bbd10e3777e8d8868ca4ad676d1e1d
Hunter R. Merrill <hunter.merrill@mac.lan>
2025-05-25 18:08:18 -0400

    a little more cleaning, more description at the top, some questions as comments in the code

--- to_archive/data-update.R
+++ to_archive/data-update.R
@@ -2,6 +2,19 @@
 This script will not work as-is. It is intended to be migrated to a Python script.
 Currently, we are editing it to remove sections that we don't think will be needed
 at inference time.
+
+Notes:
+- The original version of this script pulled data to use for training models. It
+  is being adapted to pull data for running inference with those models.
+- The script pulls data from 
+  - a Postgres database
+  - ODK (Open Data Kit) submissions
+  - Google Sheets
+
+TODO:
+- At inference time, will we need data from both the Postgres database and ODK?
+- This script also pulls five Google sheets. I've left questions in the code
+  about whether we need to do this at inference time.
 """
 
 
@@ -19,7 +32,7 @@ db <- dbPool(
   host = "taimaka-internal.org",
   port = "5432",
   user = "jostrowski",
-  password = "bobbing-tidy-untagged"
+  password = "<redacted!>"
 )
 
 current <- data.frame(db %>% tbl(in_schema("data", "current")))
@@ -84,142 +97,9 @@ current <- current %>%
 library(googlesheets4)
 library(stringr)
 
-itp_roster_2023 <- read_sheet("https://docs.google.com/spreadsheets/d/1Pb_bGGaHRIyzwHhIBzebFqQhd-6HQttWKDeTvRR58-o/edit?gid=0#gid=0") %>%
-  as.data.frame() 
+# Deleted ITP roster loading code that started from google sheets
 
-itp_roster_2023_2 <- itp_roster_2023 %>% 
-  filter(!is.na(Facility)) %>% 
-  mutate(itp = Facility,
-         otp = OTP,
-         age = as.character(`Age (months)`),
-         sex = `Sex (m/f)`,
-         initial_dx = Diagnosis,
-         outcome = Outcome,
-         outcome_date = coalesce(`Outcome date`, LOS),
-         pid = `Reg. No`,
-         los_days = as.character(`Week #`),
-         admit_date = as.Date(`Date of admission`, format = "%Y-%m-%d"),
-         outcome_date = as.Date(outcome_date, format = "%Y-%m-%d"),
-         ref_w_pid = Yes,
-         case_notes = paste(`Mgt checked in folder`, notes_2, notes_3, sep = ", "),
-         mgt_checked_folder = `Checked date`,
-         case_notes = case_notes %>%
-           str_replace_all("NA, ", "") %>%     
-           str_replace_all(", NA", "") %>%   
-           str_replace_all(", , ", ", ") %>%   
-           str_replace_all("^NA$", "") %>%
-           str_replace_all("2023-11-08", "") %>%
-           str_replace_all("2023-10-03", "") %>%
-           str_replace_all("2023-09-26", "") %>%
-           str_replace_all("2023-09-18", "")) %>% 
-  mutate(pid = if_else(pid == '-' & Name == "Mohmmed Lawadu", "23-9998", pid),
-         pid = if_else(pid == '-' & Name == "Ali Adamu", "23-9999", pid),
-         pid = if_else(pid == '-' & Name == "Laure Adamu", "23-9997", pid)) %>% 
-  select(-`Reg. No`, -`Week #`, -Name, -LOS, -`Outcome date`, -`Referred with Reg. No`,
-         -Yes, -`Mgt checked in folder`, -notes_2, -notes_3, -`Checked date`,
-         -`Date of admission`, -Facility, -OTP, -`Age (months)`, -`Sex (m/f)`,
-         -Diagnosis, -Outcome)
-
-itp_roster_2023_2$week_num <- unlist(itp_roster_2023_2$week_num)
-
-itp_roster_2024 <- read_sheet("https://docs.google.com/spreadsheets/d/1kWdcIL7ajRxHfmtS7R3BNkUzUgEac-XCWlzM4xNlkuA/edit?pli=1&gid=0#gid=0") %>%
-  as.data.frame() 
-
-itp_roster_2024$Pid[itp_roster_2024$Pid == "24--4534"] <- "24-4534"
-
-itp_roster_2024_2 <- itp_roster_2024 %>%
-  mutate(sex = `Sex (m/f)`,
-         itp = Facility,
-         otp = OTP,
-         initial_dx = `Initial Diagnosis`,
-         outcome = Outcome,
-         final_dx = `Final diagnosis`,
-         age = as.character(`Age (months)`),
-         muac = as.character(`MUAC (cm)`), 
-         pid = Pid,
-         admit_date = as.Date(`Date of Admission`, format = "%Y-%m-%d"),
-         outcome_date = as.Date(`Outcome Date`, format = "%Y-%m-%d"),
-         los_days = as.character(LOS),
-         ref_w_pid = `Referred with PID`,
-         case_notes = paste(Note, ...18, `Folder checked`, sep = ", "),
-         case_notes = case_notes %>%
-           str_replace_all("NA, ", "") %>%     
-           str_replace_all(", NA", "") %>%   
-           str_replace_all(", , ", ", ") %>%   
-           str_replace_all("^NA$", "")) %>%
-  mutate(pid = if_else(pid == '-' & Name == "Aisha Umar", "24-9999", pid),
-         pid = if_else(pid == '-' & Name == "Saifullahi Yahaya", "24-9998", pid),
-         pid = if_else(pid == '-' & Name == "Abdullahi Ismail", "24-9990", pid),
-         pid = if_else(pid == '-' & Name == "Maryam Muhd", "24-2757", pid),
-         pid = if_else(pid == '-' & Name == "zainab Tijjani", "24-9991", pid),
-         pid = if_else(pid == '-' & Name == "Hassan Usman", "24-0001", pid),
-         pid = if_else(pid == '-' & Name == "Muhd Yahaya", "24-1953", pid),
-         pid = if_else(pid == '-' & Name == "Jamila Lawai", "24-9989", pid)) %>% 
-  select(-Pid, -`Age (months)`, -`MUAC (cm)`, -`Date of Admission`, 
-         -`Outcome Date`, -LOS, -`Folder checked`, -`Referred with PID`,
-         -Note, -`?prob`, -...18, -Name, -Facility, -OTP, -`Sex (m/f)`,
-         -`Initial Diagnosis`, -`Outcome`, -`Final diagnosis`)
-
-itp_roster_2024_2$muac <- unlist(itp_roster_2024_2$muac)
-itp_roster_2024_2$age <- unlist(itp_roster_2024_2$age)
-
-itp_roster_2025 <- read_sheet("https://docs.google.com/spreadsheets/d/11LqjmNJeHNLirCaYSijpJms0Rp1NZbJDKZ0WxtSAqkk/edit?gid=0#gid=0") %>%
-  as.data.frame() 
-
-itp_roster_2025_2 <- itp_roster_2025 %>% 
-  mutate(sex = `Sex (m/f)`,
-         itp = Facility,
-         otp = OTP,
-         initial_dx = `Initial Diagnosis`,
-         outcome = Outcome,
-         final_dx = `Final diagnosis`,
-         age = as.character(`Age (months)`),
-         muac = as.character(`MUAC (cm)`), 
-         pid = Pid,
-         admit_date = as.Date(`Date of Admission`, format = "%Y-%m-%d"),
-         outcome_date = as.Date(`Outcome Date`, format = "%Y-%m-%d"),
-         los_days = as.character(LOS),
-         ref_w_pid = `Referred with PID`,
-         case_notes = paste(Note, ...18, `Folder checked`, sep = ", "),
-         case_notes = case_notes %>%
-           str_replace_all("NA, ", "") %>%     
-           str_replace_all(", NA", "") %>%   
-           str_replace_all(", , ", ", ") %>%   
-           str_replace_all("^NA$", "")) %>%
-  select(-Pid, -`Age (months)`, -`MUAC (cm)`, -`Date of Admission`, 
-         -`Outcome Date`, -LOS, -`Folder checked`, -`Referred with PID`,
-         -Note, -`?prob`, -...18, -Name, -Facility, -OTP, -`Sex (m/f)`,
-         -`Initial Diagnosis`, -`Outcome`, -`Final diagnosis`) %>% 
-  mutate(los_days = as.character(outcome_date - admit_date))
-
-itp_roster <- bind_rows(itp_roster_2024_2, itp_roster_2023_2, itp_roster_2025_2) %>% 
-  mutate(otp = ifelse(otp == 'Kurjelli', 'kurjale', otp),
-         otp = ifelse(otp == 'PHC Kurjele', 'kurjale', otp),
-         otp = ifelse(otp == 'Ashaka', 'jalingo', otp),
-         otp = ifelse(otp == 'PHC Ashaka', 'jalingo', otp),
-         otp = ifelse(otp == 'Kuri', 'kuri', otp),
-         otp = ifelse(otp == 'PHC Kuri', 'kuri', otp),
-         otp = ifelse(otp == 'Sangaru', 'sangaru', otp),
-         otp = ifelse(otp == 'PHC Sangaru', 'sangaru', otp),
-         itp = ifelse(itp == 'FTH', 'fth', itp),
-         itp = ifelse(itp == 'SSHG', 'ssh', itp),
-         itp = ifelse(itp == 'GH Deba', 'gh_deba', itp),
-         itp = ifelse(itp == 'GH Bajoga', 'gh_bajoga', itp), 
-         age = ifelse(age == '20days', '0.67', age),
-         age = ifelse(age == 'NULL', NA, age),
-         sex = ifelse(sex == 'M|', 'm', sex),
-         sex = ifelse(sex == 'M', 'm', sex),
-         sex = ifelse(sex == 'F', 'f', sex),
-         muac = ifelse(muac == '-', NA, muac),
-         muac = ifelse(muac == 'NULL', NA, muac),
-         muac = as.numeric(muac),
-         los_days = ifelse(los_days == 'NULL', NA, los_days),
-         case_notes = ifelse(los_days == 'Treated as out patient', 'Treated as out patient', case_notes),
-         los_days = ifelse(los_days == 'Treated as out patient', NA, los_days),
-         los_days = as.numeric(los_days)) %>% 
-  unique() 
-
-# Want to check-- do we need the below section?
+# TODO: Want to check-- do we need the below section?
 
 ### Additional cleaning of raw ODK admissions data ###
 admit_raw_2 <- admit_raw %>% 
@@ -371,7 +251,7 @@ weekly_raw <- odata_submission_get(
   parse=FALSE
 ) %>% odata_submission_rectangle(names_sep=NULL)
 
-# Do we need the below section?
+# TODO: Do we need staff member cleaning at inference time?
 
 weekly_raw_2 <- weekly_raw %>% 
   mutate(id = gsub("^uuid:", "", id)) %>% 
@@ -883,6 +763,8 @@ admit_raw_2$hl[admit_raw_2$hl == 31] <- 72.2
 admit_processed$finalhl[admit_processed$finalhl == 7.2] <- 82
 admit_raw_2$hl[admit_raw_2$hl == 7.2] <- 82
 
+# TODO: How to handle two visits in a single day at inference time?
+
 ### Cleaning of 2 visits in a single day ###
 pid_dup_visits <- read_sheet("https://docs.google.com/spreadsheets/d/1of2XyX_-zz-JWTNd3Xls26QPbKzmRISDshZF3OzNVBs/edit?gid=0#gid=0") %>% 
   as.data.frame() %>% 
@@ -899,73 +781,9 @@ weekly_raw_2 <- weekly_raw_2 %>%
   mutate(pid = if_else(!is.na(new_value), new_value, pid)) %>% 
   select(-new_value)
 
-weekly_processed <- weekly_processed %>% 
-  filter(uuid != "73aa0d09-7144-49e1-88e8-8c57205db19d", # corresponds to 23-1597 on Dec. 19, 2023
-         uuid != "9b449658-1d0c-481c-8276-47a607bceab5", # corresponds to 23-1850 on Dec. 29, 2023
-         uuid != "f18b0301-b961-40e0-852f-51d74dbbc209", # corresponds to 23-0559 (actually admit for 23-0554) on July 20, 2023
-         uuid != "ee475f11-b25e-4432-a7c0-9f020a5ec0b5", # corresponds to 24-0315 on April 19, 2024
-         uuid != "bc97606e-ea4f-43af-bb6c-756517ab10bd", # corresponds to 24-0999 on April 15, 2024
-         uuid != "bd5b39ea-65b1-480a-92f8-ae0276758303", # corresponds to 24-3024 on August 6, 2024
-         uuid != "325db6da-007f-4029-82da-494b3ab03169", # corresponds to 23-0379 on Aug. 18, 2023
-         uuid != "92f64f3a-dc03-4ca9-a978-4df70a93c44d", # corresponds to 23-0749 on Aug. 15, 2023
-         uuid != "fb59b4e2-86f4-41ee-9664-cd69753a00ef", # corresponds to 23-1142 on Sep. 12, 2023
-         uuid != "ce58d36d-74f7-42a7-bb71-33859f7be8b6", # corresponds to 24-3397 on Sep. 20, 2024
-         uuid != "8cc30cb7-0dfc-408a-b4cf-dd2af460fb98", # corresponds to 23-2132 on Oct. 10, 2023 
-         uuid != "394e5fbf-9065-4b09-bc6c-b765fd9b98fc", # corresponds to 24-3183 on Sep. 24, 2024, T1 (morning) (T2, in afternoon, ended in ITP referral)
-         uuid != "3750d3a2-a77d-4eef-bc48-f4464cf58699", # corresponds to 24-4503 on Oct. 25, 2024 
-         uuid != "8b3d7c2c-f788-4ba2-bfdd-e38716c3bb08", # corresponds to 23-1165 on Apr. 09, 2023
-         uuid != "99bed840-23d6-4e8e-9096-0cc23e30880b", # corresponds to 23-1429 on Oct. 16, 2023
-         uuid != "b8924426-df7d-47f2-adb2-684e093e69c0", # corresponds to 23-2909 on Jan. 02, 2024
-         uuid != "a0f0d748-e151-4041-b8e6-7e2055bf6d84", # corresponds to 23-0231 on Jun. 06, 2023
-         uuid != "d68537f3-b597-4669-9e7f-f78ef5240ceb", # corresponds to 23-0284 on Jun. 06, 2023
-         uuid != "7bd966dd-cd7e-4b6d-b4ee-15a5f4b29b4a", # corresponds to 23-0359 on Jun. 06, 2023
-         uuid != "ca49b128-d06c-4068-b957-42536be87b47", # corresponds to 23-1111 on Sep. 13, 2023
-         uuid != "51c9b45d-056b-4982-87f2-07f1d3e90aad", # corresponds to 23-1737 on Oct. 10, 2023
-         uuid != "a54f3197-e91f-4bdf-b04f-099e55a9c520", # corresponds to 23-1809 on Nov. 16, 2023
-         uuid != "cd0e2916-abec-4c30-bbee-24d1225ebaec", # corresponds to 23-2917 on Nov. 21, 2023
-         uuid != "5d24e7b1-a2fd-4b4c-92a6-708ca3d25546", # corresponds to 24-0757 on Mar. 07, 2024
-         uuid != "d24520c1-cf3e-41fe-9eb4-adcb901253f9", # corresponds to 24-3539 on Nov. 08, 2024
-         uuid != "c75495e2-1d09-4fa8-9432-78ed3c452718", # corresponds to 24-3728 on Oct. 11, 2024
-         uuid != "458ec137-d9ee-4c54-b217-e9332ef4a054", # corresponds to 24-3729 on Oct. 11, 2024
-         uuid != "3619fa38-d4c1-49b8-a2d6-30e6bf07ee6a", # corresponds to 23-1259 on Sep. 25, 2023
-         uuid != "5cf1b4c2-48bc-499c-b111-02f442f63ccc", # corresponds to 23-0897 on Jul. 31, 2023, filtered this spike in MUAC and weight
-         uuid != "74ea71a0-a6ab-4b45-bc9b-1acc334cbdae") # corresponds to 23-3261 on Jan. 24, 2024
+# Removed historical filtering of weekly processed data by UUID
 
-
-weekly_raw_2 <- weekly_raw_2 %>% 
-  filter(id != "73aa0d09-7144-49e1-88e8-8c57205db19d", # corresponds to 23-1597 on Dec. 19, 2023
-         id != "9b449658-1d0c-481c-8276-47a607bceab5", # corresponds to 23-1850 on Dec. 29, 2023
-         id != "f18b0301-b961-40e0-852f-51d74dbbc209", # corresponds to 23-0559 (actually admit for 23-0554) on July 20, 2023
-         id != "ee475f11-b25e-4432-a7c0-9f020a5ec0b5", # corresponds to 24-0315 on April 19, 2024
-         id != "bc97606e-ea4f-43af-bb6c-756517ab10bd", # corresponds to 24-0999 on April 15, 2024
-         id != "bd5b39ea-65b1-480a-92f8-ae0276758303", # corresponds to 24-3024 on August 6, 2024
-         id != "325db6da-007f-4029-82da-494b3ab03169", # corresponds to 23-0379 on Aug. 18, 2023
-         id != "92f64f3a-dc03-4ca9-a978-4df70a93c44d", # corresponds to 23-0749 on Aug. 15, 2023
-         id != "fb59b4e2-86f4-41ee-9664-cd69753a00ef", # corresponds to 23-1142 on Sep. 12, 2023
-         id != "ce58d36d-74f7-42a7-bb71-33859f7be8b6", # corresponds to 24-3397 on Sep. 20, 2024
-         id != "8cc30cb7-0dfc-408a-b4cf-dd2af460fb98", # corresponds to 23-2132 on Oct. 10, 2023 
-         id != "394e5fbf-9065-4b09-bc6c-b765fd9b98fc", # corresponds to 24-3183 on Sep. 24, 2024, T1 (morning) (T2, in afternoon, ended in ITP referral)
-         id != "3750d3a2-a77d-4eef-bc48-f4464cf58699", # corresponds to 24-4503 on Oct. 25, 2024 
-         id != "8b3d7c2c-f788-4ba2-bfdd-e38716c3bb08", # corresponds to 23-1165 on Apr. 09, 2023
-         id != "99bed840-23d6-4e8e-9096-0cc23e30880b", # corresponds to 23-1429 on Oct. 16, 2023
-         id != "b8924426-df7d-47f2-adb2-684e093e69c0", # corresponds to 23-2909 on Jan. 02, 2024
-         id != "a0f0d748-e151-4041-b8e6-7e2055bf6d84", # corresponds to 23-0231 on Jun. 06, 2023
-         id != "d68537f3-b597-4669-9e7f-f78ef5240ceb", # corresponds to 23-0284 on Jun. 06, 2023
-         id != "7bd966dd-cd7e-4b6d-b4ee-15a5f4b29b4a", # corresponds to 23-0359 on Jun. 06, 2023
-         id != "ca49b128-d06c-4068-b957-42536be87b47", # corresponds to 23-1111 on Sep. 13, 2023
-         id != "51c9b45d-056b-4982-87f2-07f1d3e90aad", # corresponds to 23-1737 on Oct. 10, 2023
-         id != "a54f3197-e91f-4bdf-b04f-099e55a9c520", # corresponds to 23-1809 on Nov. 16, 2023
-         id != "cd0e2916-abec-4c30-bbee-24d1225ebaec", # corresponds to 23-2917 on Nov. 21, 2023
-         id != "5d24e7b1-a2fd-4b4c-92a6-708ca3d25546", # corresponds to 24-0757 on Mar. 07, 2024
-         id != "d24520c1-cf3e-41fe-9eb4-adcb901253f9", # corresponds to 24-3539 on Nov. 08, 2024
-         id != "c75495e2-1d09-4fa8-9432-78ed3c452718", # corresponds to 24-3728 on Oct. 11, 2024
-         id != "458ec137-d9ee-4c54-b217-e9332ef4a054", # corresponds to 24-3729 on Oct. 11, 2024
-         id != "3619fa38-d4c1-49b8-a2d6-30e6bf07ee6a", # corresponds to 23-1259 on Sep. 25, 2023
-         id != "5cf1b4c2-48bc-499c-b111-02f442f63ccc", # corresponds to 23-0897 on Jul. 31, 2023, filtered this spike in MUAC and weight
-         id != "74ea71a0-a6ab-4b45-bc9b-1acc334cbdae") # corresponds to 23-3261 on Jan. 24, 2024
-
-# For errors, run:
-# googlesheets4::gs4_deauth()
+# TODO: do we need a weekly age update at inference time?
 
 # Incorporate weekly age updates from google spreadsheet/csv
 weekly_age_update <- read_sheet("https://docs.google.com/spreadsheets/d/1f34ZlqbOt5nyY-TbVvpX3BBtIcCBDWsg7bwFTYadhpw/edit?usp=sharing") %>%
@@ -995,6 +813,8 @@ weekly_raw_2 <- weekly_raw_2 %>%
 # Incorporate weekly extremes updates from google spreadsheet/csv
 library(tidyverse)
 library(googlesheets4)
+
+# TODO: do we need weekly extremes updates at inference time?
 
 # Read and preprocess the weekly_extremes data
 weekly_extremes <- read_sheet("https://docs.google.com/spreadsheets/d/1v6ZjgaZ-TZOzoVi3HdBCzNr4ec-Omg7SmZ379-STzXc/edit?usp=sharing") %>%
@@ -1210,6 +1030,7 @@ weekly_raw_2 <- weekly_raw_2 %>%
   ) %>%
   select(-to_change, -new_value_c, -new_value_n)
 
+# TODO: do we need admit extremes updates at inference time?
 
 # Incorporate admit extremes updates from google spreadsheet/csv
 admit_extremes <- read_sheet("https://docs.google.com/spreadsheets/d/1aaLUNUBK0Run9A999EZpVBTT77mnwRKP9u3GrDwoHlM/edit?gid=1821666321#gid=1821666321") %>% 
@@ -1385,6 +1206,7 @@ admit_raw_2 <- admit_raw_2 %>%
   ) %>%
   select(-to_change, -new_value_n)
 
+# TODO: do we need birthdate and age updates at inference time?
 
 #birthdate and subsequent age changes
 birthdate_changes <- read_sheet("https://docs.google.com/spreadsheets/d/1l_Xm4UeWqZNs5z5_QVS7pvHVSrCgOiLe6B3hcY7hIHw/edit?gid=0#gid=0") %>% 
