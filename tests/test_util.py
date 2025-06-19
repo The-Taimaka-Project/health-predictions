4aa73adfa66989af9e320bf6d003b9baf7aa59e3
Brian Chaplin <brian.chaplin@gmail.com>
2025-06-19 12:31:27 -0400

    Update test_util.py

--- tests/test_util.py
+++ tests/test_util.py
@@ -7,7 +7,7 @@ from packages.inference.run import util
 import unittest
 from unittest.mock import patch, MagicMock
 import pandas as pd
-from google.colab import drive
+#from google.colab import drive
 
 class TestEtlReaderWriter(unittest.TestCase):
 
