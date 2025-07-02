c1a44f402a23cf3b4b77906278726b2ceed65f45
Brian Chaplin <brian.chaplin@gmail.com>
2025-07-02 08:29:23 -0400

    Update infer.py

--- taimaka_health_predictions/inference/infer.py
+++ taimaka_health_predictions/inference/infer.py
@@ -188,6 +188,7 @@ pid_probabilities[f'percentrank_{label}_stratified'] = pid_probabilities[f'proba
 top_pct_pids = pid_probabilities[pid_probabilities[f'percentrank_{label}_stratified'] > TOP_PCT]['pid'].unique()
 
 if EXPORT_SHAP_WATERFALL:
+  print('detn_admit_only shape',detn_admit_only[(detn_admit_only['pid'].isin(active_pids)) & (detn_admit_only[label]== 0)].shape)
   # json_series = export_waterfall_shap_values(explainer1a,detn_admit_only[(detn_admit_only['pid'].isin(active_pids)) & (detn_admit_only['pid'].isin(top_pct_pids)) & (detn_admit_only[label]== 0)],ag_features1a)
   json_series = export_waterfall_shap_values(explainer1a,detn_admit_only[(detn_admit_only['pid'].isin(active_pids)) & (detn_admit_only[label]== 0)],ag_features1a)  
   json_series2 = export_waterfall_shap_values(explainer2a,detn_filtered[(detn_filtered['pid'].isin(active_pids)) & (detn_filtered['pid'].isin(top_pct_pids)) & (detn_filtered[label]== 0)],ag_features2a)
